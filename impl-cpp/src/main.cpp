d954a54f6ce04fc89e2e91513981a4d1c2f466de
mintzis <mariosmintzis@hotmail.com>
2016-02-25 01:24:01 +0000

    BFS gives priority to the vertex with less adjacent

--- impl-cpp/src/main.cpp
+++ impl-cpp/src/main.cpp
@@ -1,122 +1,101 @@
 #include <Timer.hpp>
 
+#include "include/cpp_btree/btree_map.h"
+#include "include/cpp_btree/btree_set.h"
+
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 #include <memory>
 #include <new>
-#include <vector>
-#include <algorithm>
 #include <sstream>
+#include <string>
 
 cy::Timer_t timer;
 
-using EdgeContainer = std::vector<int>;
+template<typename K, typename V>
+using Map = btree::btree_map<K, V>;
+template<typename K, typename V>
+using Set = btree::btree_set<K, V>;
+
+template<typename T>
+using IntMap = btree::btree_map<int, T>;
+//using IntMap = std::unordered_map<int, T>;
+
+using IntSet = btree::btree_set<int>;
+
+//using EdgeContainer = IntMap<bool>;
+using EdgeContainer = IntSet;
 
 struct Vertex {
 	EdgeContainer E;
 };
 
-using VertexContainer = std::unordered_map<int, Vertex*>;
+using VertexContainer = IntMap<Vertex*>;
 
 struct Graph {
 	VertexContainer V;
-        VertexContainer Vpred;
-	bool init=true;
+	VertexContainer Vpred;
+	int count=0;
 
 	void addEdge(int from, int to) {
 		VertexContainer::iterator v;
-
 		if ((v = V.find(from)) == V.end()) {
-		v = V.insert(v, {from, new Vertex()});
+			v = V.insert(v, {from, new Vertex()});
 		}
-			if(init){
-			std::vector<int>::iterator position = std::find(v->second->E.begin(), v->second->E.end(), to);
-                		if (position == v->second->E.end())
-				v->second->E.push_back(to);
-			}
-			else{
-			v->second->E.push_back(to);
-			}
+		v->second->E.insert(to);
+
 		if ((v = Vpred.find(to)) == Vpred.end()) {
 			v = Vpred.insert(v, {to, new Vertex()});
 		}
-		if(init){
-			std::vector<int>::iterator position = std::find(v->second->E.begin(), v->second->E.end(), to);
-                		if (position == v->second->E.end())
-				v->second->E.push_back(from);
-			}
-			else{
-			v->second->E.push_back(from);
-			}
-		}
+		v->second->E.insert(from);
+	}
 
-		void removeEdge(int from, int to) {
+	void removeEdge(int from, int to) {
 		VertexContainer::iterator it;
 		if ((it = V.find(from)) != V.end()) {
-		std::vector<int>::iterator position = std::find(it->second->E.begin(), it->second->E.end(), to);
-if (position != it->second->E.end()) 
-    it->second->E.erase(position);
-			}
+			it->second->E.erase(to);
+		}
+
 		if ((it = Vpred.find(to)) != Vpred.end()) {
-			std::vector<int>::iterator position = std::find(it->second->E.begin(), it->second->E.end(), from);
-if (position != it->second->E.end()) 
-    it->second->E.erase(position);
+			it->second->E.erase(from);
 		}
-
 	}
 
-	int singleBFS(int from, int to) {
-		auto Q = std::vector<int>();
-		Q.reserve(32);
-		size_t Qidx = 0;
-		auto visited = std::unordered_map<int, int>();
-		visited[from] = 0;
-
-		Vertex *v;
-		Q.push_back(from);
-		for (;Qidx < Q.size();) {
-			int vid = Q[Qidx++];
-
-			int newDepth = visited[vid] + 1;
-			v = V[vid];
-			for (const auto &e : v->E) {
-				int k = e;
-				if (visited.find(k) == visited.end()) {
-					if (k == to) {
-						return newDepth;
-					}
-					visited[k] = newDepth;
-					if (V.find(k) == V.end()) {
-						continue;
-					}
-					Q.push_back(k);
-				}
-			}
-		}
-		return -1;
-	}
-
-int biBFS(int from, int to) {
+	int biBFS(int from, int to) {
 		auto QF = std::vector<int>(); QF.reserve(64);
 		auto QT = std::vector<int>(); QF.reserve(64);
 
-		auto visitedF = std::unordered_map<int, int>();
+		auto visitedF = Map<int, int>();
 		visitedF[from] = 0;
 		size_t QFidx = 0;
 
-		auto visitedT = std::unordered_map<int, int>();
+		auto visitedT = Map<int, int>();
 		visitedT[to] = 0;
 		size_t QTidx = 0;
 
 		Vertex *v;
 		QF.push_back(from);
 		QT.push_back(to);
+
+		int vidF,vidT,fromN,toN;
+					
 		while (QFidx < QF.size() && QTidx < QT.size()) {
 
-			if ((QF.size()-QFidx) < (QT.size()-QTidx)) {
+				vidF = QF[QFidx];
+				v = V[vidF];
+				fromN=v->E.size();
+				
+				vidT = QT[QTidx];
+				v = Vpred[vidT];
+				toN=v->E.size();
+
+				if(fromN<=toN){
 				size_t this_level = QF.size();
+				
 				for (;QFidx < this_level;) {
 					int vid = QF[QFidx++];
+
 					int newDepth = visitedF[vid] + 1;
 
 					v = V[vid];
@@ -173,13 +152,15 @@ int biBFS(int from, int to) {
 		if (V.find(from) == V.end()) {
 			return -1;
 		}
-
 		if (Vpred.find(to) == Vpred.end()) {
 			return -1;
 		}
+
+		
+	
+		//return singleBFS(from, to);
 		return biBFS(from, to);
 	}
-
 };
 
 std::unique_ptr<Graph> readInitialGraph() {
@@ -196,7 +177,6 @@ std::unique_ptr<Graph> readInitialGraph() {
 			break;
 		}
 		if (line == "S") {
-			G->init=true;
 			std::cerr << "loadedInitialGraph:: " << timer.getChrono(start) << std::endl;
 			break;
 		}
@@ -211,16 +191,23 @@ std::unique_ptr<Graph> readInitialGraph() {
 	return std::move(G);
 }
 
+uint64_t tQ = 0, tA = 0, tD = 0;
 void executeOperation(Graph &G, char c, int from, int to) {
+	auto start = timer.getChrono();
 	switch (c) {
 	case 'Q':
+		
+		
 		std::cout << G.BFS(from, to) << std::endl;
+		tQ += timer.getChrono(start);
 		break;
 	case 'A':
 		G.addEdge(from, to);
+		tA += timer.getChrono(start);
 		break;
 	case 'D':
 		G.removeEdge(from, to);
+		tD += timer.getChrono(start);
 		break;
 	}
 }
@@ -231,7 +218,8 @@ void runExecution(Graph &G) {
 	std::string line;
 	std::stringstream ss;
 
-	auto start = timer.getChrono();	
+	uint64_t start = timer.getChrono();
+	uint64_t lastEnd = 0;
 	for(;;) {
 		//std::cerr << "reading..." << std::endl;
 		if (!std::getline(std::cin, line)) {
@@ -239,7 +227,9 @@ void runExecution(Graph &G) {
 		}
 		//std::cerr << "read: " << line << std::endl;
 		if (line == "F") {
-			std::cerr << "runExecutionBatch:: " << timer.getChrono(start) << std::endl;
+			uint64_t current = timer.getChrono(start) - lastEnd;
+			lastEnd += current;
+			std::cerr << "runExecutionBatch:: " << current << std::endl;
 			continue;
 		}
 		ss.clear();
@@ -252,7 +242,7 @@ void runExecution(Graph &G) {
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
+	std::ios_base::sync_with_stdio(false);
     setvbuf(stdin, NULL, _IOFBF, 1<<20);
 
     auto start = timer.getChrono();
@@ -260,4 +250,6 @@ int main() {
     std::cerr << "readInitialGraph:: " << timer.getChrono(start) << std::endl;
 
     runExecution(*G);
+
+    std::cerr << "tQ: " << tQ << " tA: " << tA << " tD: " << tD << std::endl;
 }
