82a2ef75da1e04518912f9b5dc5d82b3b242dbb8
mintzis <mariosmintzis@hotmail.com>
2016-03-01 22:17:24 +0000

    Components implementation  (Broken)

--- impl-cpp/src/main.cpp
+++ impl-cpp/src/main.cpp
@@ -29,6 +29,7 @@ using EdgeContainer = IntSet;
 
 struct Vertex {
 	EdgeContainer E;
+	int * componentId;
 };
 
 using VertexContainer = IntMap<Vertex*>;
@@ -36,19 +37,202 @@ using VertexContainer = IntMap<Vertex*>;
 struct Graph {
 	VertexContainer V;
 	VertexContainer Vpred;
-	int count=0;
+	int componentsIds=-1;
+	 int components[20000000] ;//= new int[200000]; 
+	
 
 	void addEdge(int from, int to) {
-		VertexContainer::iterator v;
-		if ((v = V.find(from)) == V.end()) {
-			v = V.insert(v, {from, new Vertex()});
+		VertexContainer::iterator vFrom =V.find(from);
+		VertexContainer::iterator vTo=Vpred.find(to);
+		VertexContainer::iterator vPredFrom=Vpred.find(from);
+		VertexContainer::iterator vOrigTo=V.find(to);
+		
+		//from oxi mesa
+		if(vFrom == V.end())
+		{
+			//both need to add
+			if(vTo==Vpred.end())
+			{
+				//cboth nowhere
+				if(vPredFrom==Vpred.end() && vOrigTo==V.end())
+				{
+					Vertex *v2=new Vertex();
+					++componentsIds;
+					//dixnw stin thesi
+					////std::cerr<<"before "<<components[componentsIds];
+					components[componentsIds]=componentsIds;
+					
+					v2->componentId=&components[componentsIds];//componentsIds;
+					
+					Vertex *v3=new Vertex();
+					v3->componentId=&components[componentsIds];
+					vFrom = V.insert(vFrom, {from, v2});
+					vTo = Vpred.insert(vTo, {to, v3});
+					
+					//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v2->componentId)<<" eerwww "<<*(v3->componentId)<<std::endl;
+					
+				}
+				//to is in the original from is nowhere
+				else if(vPredFrom==Vpred.end() && vOrigTo!=V.end())
+				{
+					Vertex *v2=new Vertex();
+					v2->componentId=vOrigTo->second->componentId;
+					Vertex *v3=new Vertex();
+					v3->componentId=vOrigTo->second->componentId;
+					vFrom = V.insert(vFrom, {from, v2});
+					vTo = Vpred.insert(vTo, {to, v3});
+					//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v2->componentId)<<" lloooo "<<*(v3->componentId)<<std::endl;
+				}
+				else if(vPredFrom!=Vpred.end()&& vOrigTo==V.end())
+				{
+					Vertex *v2=new Vertex();
+					v2->componentId=vPredFrom->second->componentId;
+					Vertex *v3=new Vertex();
+					v3->componentId=vPredFrom->second->componentId;
+					vFrom = V.insert(vFrom, {from, v2});
+					vTo = Vpred.insert(vTo, {to, v3});
+					//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v2->componentId)<<" srrr "<<*(v3->componentId)<<std::endl;
+				}
+				//both stous anapodous (mporei na thelei allo solution dame)
+				else{
+					
+					//prepei na pianoume tsino me ta pio lia pethkia level 1
+					if(vOrigTo->second->componentId != vPredFrom->second->componentId)
+					{*(vPredFrom->second->componentId)=*(vOrigTo->second->componentId);
+						////std::cerr<<" case "<<*(vPredFrom->second->componentId)<<" tp "<<*(vOrigTo->second->componentId)<<std::endl;
+						//components[*(vPredFrom->second->componentId)]=components[*(vOrigTo->second->componentId)];	
+					}	
+					
+					//TODO:pou tha dixnoun meta?
+					Vertex *v2=new Vertex();
+					v2->componentId=vOrigTo->second->componentId;;//vOrigTo->second->componentId;
+					Vertex *v3=new Vertex();
+					v3->componentId=vOrigTo->second->componentId;
+					vFrom = V.insert(vFrom, {from, v2});
+					vTo = Vpred.insert(vTo, {to, v3});
+					//std::cerr<<" from  "<<from<<" towww "<<to<<" "<<*(v2->componentId)<<" "<<*(v3->componentId)<<std::endl;
+				}
+				
+			}
+			//from den ine mesa ston kanoniko tou prepei na mpei alla o to ine
+			else{
+				//TO EN MESA
+				
+								if(vPredFrom==Vpred.end() && vOrigTo==V.end())
+								{
+													Vertex *v3=new Vertex();
+													v3->componentId=vTo->second->componentId;
+													vFrom = V.insert(vFrom, {from, v3});
+													//std::cerr<<" from  "<<from<<" to "<<to<<" assse "<<*(v3->componentId)<<std::endl;
+								}
+								//to is in the original from is nowhere
+								else if(vPredFrom==Vpred.end() && vOrigTo!=V.end())
+								{
+									Vertex *v3=new Vertex();
+									v3->componentId=vTo->second->componentId;
+									//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v3->componentId)<<" 2fix originTo "<<*(vOrigTo->second->componentId)<<std::endl;
+									*(vOrigTo->second->componentId)=*(vTo->second->componentId);
+									vFrom = V.insert(vFrom, {from, v3});	
+									
+								}
+								else if(vPredFrom!=Vpred.end()&& vOrigTo==V.end())
+								{
+									Vertex *v3=new Vertex();
+									v3->componentId=vTo->second->componentId;
+									//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v3->componentId)<<" 3fix PredFrom "<<*(vPredFrom->second->componentId)<<std::endl;
+									*(vPredFrom->second->componentId)=*(vTo->second->componentId);
+									vFrom = V.insert(vFrom, {from, v3});		
+								}
+								//both stous anapodous (mporei na thelei allo solution dame)
+								else{
+									//prepei na pianoume tsino me ta pio lia pethkia level 1
+									Vertex *v3=new Vertex();
+									v3->componentId=vTo->second->componentId;
+									//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v3->componentId)<<" 4fix PredFrom "<<*(vPredFrom->second->componentId)<<" orig "<<*(vOrigTo->second->componentId)<<std::endl;
+									*(vPredFrom->second->componentId)=*(vTo->second->componentId);
+									*(vOrigTo->second->componentId)=*(vTo->second->componentId);
+									vFrom = V.insert(vFrom, {from, v3});		
+								}
+			
+	
+			}
+			
+			
 		}
-		v->second->E.insert(to);
-
-		if ((v = Vpred.find(to)) == Vpred.end()) {
-			v = Vpred.insert(v, {to, new Vertex()});
+		//from mesa
+		else
+		{
+			//[prepei na mpei o TO
+			if(vTo==Vpred.end())
+						{
+							
+								if(vPredFrom==Vpred.end() && vOrigTo==V.end())
+								{
+					
+									Vertex *v3=new Vertex();
+									v3->componentId=vFrom->second->componentId;
+									vTo = Vpred.insert(vTo, {to, v3});
+									//std::cerr<<" from  "<<from<<" to "<<to<<" aa  "<<*(v3->componentId)<<std::endl;
+								
+								}
+								else if(vPredFrom!=Vpred.end() && vOrigTo==V.end())
+								{
+									Vertex *v3=new Vertex();
+									v3->componentId=vFrom->second->componentId;
+									//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v3->componentId)<<" 5fix predfrom " <<*(vPredFrom->second->componentId)<<std::endl;
+									*(vPredFrom->second->componentId)=*(vFrom->second->componentId);
+									vTo = Vpred.insert(vTo, {to, v3});		
+									
+								}
+								else if(vPredFrom==Vpred.end() && vOrigTo!=V.end())
+								{
+									Vertex *v3=new Vertex();
+																		v3->componentId=vFrom->second->componentId;
+																		//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v3->componentId)<<" fix orig" <<*(vOrigTo->second->componentId)<<std::endl;
+																		*(vOrigTo->second->componentId)=*(vFrom->second->componentId);
+																		vTo = Vpred.insert(vTo, {to, v3});		
+								}
+								else{
+									Vertex *v3=new Vertex();
+																		v3->componentId=vFrom->second->componentId;
+																		//std::cerr<<" from  "<<from<<" to "<<to<<" "<<*(v3->componentId)<<" fix PredFrom "<<*(vPredFrom->second->componentId)<<" orig "<<*(vOrigTo->second->componentId)<<std::endl;
+																		*(vPredFrom->second->componentId)=*(vFrom->second->componentId);
+																		*(vOrigTo->second->componentId)=*(vFrom->second->componentId);
+																		vTo = Vpred.insert(vTo, {to, v3});		
+																		
+								}
+				
+				
+								
+						}
+			else{
+				//both mesa
+								
+							if(*(vFrom->second->componentId)!=*(vTo->second->componentId))
+								
+							*(vFrom->second->componentId)=*(vTo->second->componentId);
+//							if(vFrom->second->E.size()>0)
+//															{
+//																
+//																for (const auto& e : vFrom->second->E)
+//																{
+//																	Vertex * v=Vpred[e];
+//																	std::cerr<<"      inside "<<(*v->componentId)<<std::endl;
+//																}
+//															}
+								//vFrom->second->componentId=vTo->second->componentId;
+								
+			}
+			
+			
 		}
-		v->second->E.insert(from);
+		
+		
+		
+		
+		
+		vFrom->second->E.insert(to);
+		vTo->second->E.insert(from);
 	}
 
 	void removeEdge(int from, int to) {
@@ -61,6 +245,8 @@ struct Graph {
 			it->second->E.erase(from);
 		}
 	}
+	
+	
 
 	int biBFS(int from, int to) {
 		auto QF = std::vector<int>(); QF.reserve(64);
@@ -111,7 +297,7 @@ struct Graph {
 						}
 						it = visitedT.find(k);
 						if (it != visitedT.end()){
-							//std::cerr << newDepth << ":" << from << ":" << to << " : " << k << std::endl;
+							////std::cerr << newDepth << ":" << from << ":" << to << " : " << k << std::endl;
 							return it->second + newDepth;
 						}
 					}
@@ -149,11 +335,25 @@ struct Graph {
 		if (from == to) {
 			return 0;
 		}
-		if (V.find(from) == V.end()) {
+		VertexContainer::iterator vFrom;
+		VertexContainer::iterator vTo;
+		if ((vFrom=V.find(from)) == V.end()) {
 			return -1;
 		}
-		if (Vpred.find(to) == Vpred.end()) {
+		if ((vTo=Vpred.find(to)) == Vpred.end()) {
 			return -1;
+		}
+		
+		
+		if(*(vFrom->second->componentId)!=*(vTo->second->componentId) )
+		{//std::cerr<<"idfrom "<<from<<" "<<*(vFrom->second->componentId)<<" to "<<to<<" "<<*(vTo->second->componentId)<< " -1 actuall " << biBFS(from, to)<<std::endl;
+			////std::cerr<<"-1"<<std::endl;
+			return -1;
+			
+			
+		}
+		else{
+			////std::cerr<<"idfrom "<<*(vFrom->second->componentId)<<" to "<<*(vTo->second->componentId)<< " -1 actuall " << biBFS(from, to)<<std::endl;	
 		}
 
 		
@@ -173,11 +373,11 @@ std::unique_ptr<Graph> readInitialGraph() {
 
 	for(;;) {
 		if (!std::getline(std::cin, line)) {
-			std::cerr << "error" << std::endl;
+			//std::cerr << "error" << std::endl;
 			break;
 		}
 		if (line == "S") {
-			std::cerr << "loadedInitialGraph:: " << timer.getChrono(start) << std::endl;
+			//std::cerr << "loadedInitialGraph:: " << timer.getChrono(start) << std::endl;
 			break;
 		}
 		ss.clear();
@@ -221,11 +421,11 @@ void runExecution(Graph &G) {
 	uint64_t start = timer.getChrono();
 	uint64_t lastEnd = 0;
 	for(;;) {
-		//std::cerr << "reading..." << std::endl;
+		////std::cerr << "reading..." << std::endl;
 		if (!std::getline(std::cin, line)) {
 			break;
 		}
-		//std::cerr << "read: " << line << std::endl;
+		////std::cerr << "read: " << line << std::endl;
 		if (line == "F") {
 			uint64_t current = timer.getChrono(start) - lastEnd;
 			lastEnd += current;
@@ -236,7 +436,7 @@ void runExecution(Graph &G) {
 		ss.str(line);
 		ss >> c >> from >> to;
 
-		//std::cerr << "execute:: " << c << ":" << from << ":" << to << std::endl;
+		////std::cerr << "execute:: " << c << ":" << from << ":" << to << std::endl;
 		executeOperation(G, c, from, to);
 	}
 }
@@ -247,9 +447,9 @@ int main() {
 
     auto start = timer.getChrono();
     std::unique_ptr<Graph> G = readInitialGraph();
-    std::cerr << "readInitialGraph:: " << timer.getChrono(start) << std::endl;
+    //std::cerr << "readInitialGraph:: " << timer.getChrono(start) << std::endl;
 
     runExecution(*G);
 
-    std::cerr << "tQ: " << tQ << " tA: " << tA << " tD: " << tD << std::endl;
+    //std::cerr << "tQ: " << tQ << " tA: " << tA << " tD: " << tD << std::endl;
 }
